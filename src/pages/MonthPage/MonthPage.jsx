2c9d796efb121ed6e51397bfe794205122a1526e
Erball-M <119080597+Erball-M@users.noreply.github.com>
2023-06-29 11:32:25 +0600

    default params for MonthPage-IndexRoute

--- src/pages/MonthPage/MonthPage.jsx
+++ src/pages/MonthPage/MonthPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import cl from './MonthPage.module.scss'
 
 const MonthPage = () => {
+    const {
+        year = (new Date().getFullYear()),
+        month = (new Date().getMonth() + 1),
+    } = useParams()
+
     return (
-        <div>MonthPage</div>
+        <div>MonthPage {month}.{year}</div>
     )
 }
 
