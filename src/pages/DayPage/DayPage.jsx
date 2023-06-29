8a01d01b9f2f1659e107702c8b97e6ed31238053
Erball-M <119080597+Erball-M@users.noreply.github.com>
2023-06-29 11:50:49 +0600

    loging page params for testing

--- src/pages/DayPage/DayPage.jsx
+++ src/pages/DayPage/DayPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import cl from './DayPage.module.scss'
 
 const DayPage = () => {
+    const { year, month, day } = useParams()
+
     return (
-        <div>DayPage</div>
+        <div>DayPage {day}.{month}.{year}</div>
     )
 }
 
--- src/pages/WeekPage/WeekPage.jsx
+++ src/pages/WeekPage/WeekPage.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 import cl from './WeekPage.module.scss'
 
 const WeekPage = () => {
+    const { year, month, week } = useParams()
+
     return (
-        <div>WeekPage</div>
+        <div>WeekPage {month}.{year} week {week}</div>
     )
 }
 
