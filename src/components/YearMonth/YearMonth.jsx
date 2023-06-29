330b1bed2951a4e54da09346a494ae18b0504cd3
Erball-M <119080597+Erball-M@users.noreply.github.com>
2023-06-29 12:51:41 +0600

    General Styles of YearPage done TEMPORARY

--- src/components/YearMonth/YearMonth.jsx
+++ src/components/YearMonth/YearMonth.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import { getDaysWithIndent } from '../../utils/yearUtils'
+import { WeekDayNames } from '../../constans/dateNames'
 import cl from './YearMonth.module.scss'
 
-const YearMonth = () => {
+const WeekDaysRow = () => {
     return (
-        <div>YearMonth</div>
+        <>
+            {WeekDayNames.map(weekDay => (
+                <div
+                    key={weekDay}
+                    className={cl.day}
+                >
+                    {weekDay}
+                </div>
+            ))}
+        </>
+    )
+}
+
+const YearMonth = ({ month }) => {
+    const hadnledMonthDays = useMemo(() => {
+        return (getDaysWithIndent(month))
+    }, [month])
+
+    return (
+        <div className={cl.month}>
+            <h3 className={cl.name}>{month.name}</h3>
+            <div className={cl.grid}>
+                <WeekDaysRow />
+                {hadnledMonthDays.map(day => (
+                    <div key={day?.date || day} className={cl.day}>
+                        {day?.date || ''}
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
--- src/pages/YearPage/YearPage.jsx
+++ src/pages/YearPage/YearPage.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useParams } from 'react-router-dom'
+import { getYearMonths } from '../../utils/yearUtils'
 import cl from './YearPage.module.scss'
+import { YearMonth } from '../../components/components'
 
 const YearPage = () => {
   const { year } = useParams()
+  const months = useMemo(() => {
+    return (getYearMonths(year))
+  }, [year])
 
   return (
     <div className={cl.wrapper}>
       <div className={cl.grid}>
-
+        {months.map(month => (<YearMonth key={month.name} month={month} />))}
       </div>
     </div>
   )
