bcda005fd6077e67b5aa4f6733c3ec0cb7761dab
Erball-M <119080597+Erball-M@users.noreply.github.com>
2023-06-29 14:42:43 +0600

    refactored and merged to dateUtils

---
+++ src/utils/dateUtils.js
@@ -0,0 +1,94 @@
+import { MonthNames } from '../constans/dateNames'
+import { YearRange, MonthRange } from '../constans/availableRanges'
+
+
+export function getYearMonths(year) {
+    const y = parseInt(year)
+    const res = []
+    for (let i = 0; i < MonthNames.length; i++) {
+        res.push(getMonthDays(y, i))
+    }
+    return res
+}
+
+export function getMonthDays(year, month) {
+    const handledMonth = month - 1
+    const name = MonthNames[handledMonth]
+    const daysCount = new Date(year, handledMonth + 1, 0).getDate()
+    const weekDayStartNumber = new Date(year, handledMonth).getDay()
+    const weekDayStart = weekDayStartNumber === 0 ? 6 : weekDayStartNumber - 1
+    const days = []
+    for (let i = 0; i < daysCount; i++) {
+        const day = {
+            date: i + 1,
+            notices: [],
+        }
+        days.push(day)
+    }
+    return ({
+        name,
+        days,
+        daysCount,
+        indent: weekDayStart,
+    })
+}
+
+export function getMonthDaysWithIndent(month) {
+    const indentDays = []
+    for (let i = 0; i < month.weekDayStart; i++) {
+        indentDays.push(`indentDay${i}`)
+    }
+    return [...indentDays, ...month.days]
+}
+
+export function checkDateRange(params) {
+    const year = parseInt(params.year)
+    const month = parseInt(params.month)
+    const day = parseInt(params.day)
+
+    const res = ''
+    if (year ?? false) {
+        const yearInRange = checkYearInRange(year)
+        res += yearInRange
+    }
+    if (month ?? false) {
+        const monthInRange = checkMonthInRange(month)
+        res += monthInRange
+    }
+    if (day ?? false) {
+
+    }
+    return res
+}
+
+function checkYearInRange(year) {
+    const y = parseInt(year)
+    const [from, to] = YearRange
+    if (isNaN(y)) {
+        return `/${new Date().getFullYear()}`
+    } else if (y >= from && y <= to) {
+        return `${year}`
+    } else if (y < from) {
+        return `/${from}`
+    } else if (y > to) {
+        return `/${to}`
+    }
+}
+
+function checkMonthInRange(year, month) {
+    const y = parseInt(year)
+    const isCurrentYear = new Date().getFullYear() === y
+    const m = parseInt(month)
+    const [from, to] = MonthRange
+
+    if (isNaN(m) || m < from || m > to) {
+        if (isCurrentYear) {
+            const currentMonth = new Date().getMonth() + 1
+            return `/${currentMonth}`
+        } else {
+            return `/1`
+        }
+    } else {
+        return `/${month}`
+    }
+}
--- src/utils/yearUtils.js
+++ src/utils/yearUtils.js
@@ -1,48 +1,50 @@
-import { MonthNames } from '../constans/dateNames'
-import { yearRange } from '../constans/availableRanges'
+// import { MonthNames } from '../constans/dateNames'
+// import { YearRange } from '../constans/availableRanges'
 
-export const getYearMonths = (year) => {
-    const res = []
-    for (let i = 0; i < MonthNames.length; i++) {
-        const name = MonthNames[i]
-        const daysCount = new Date(year, i + 1, 0).getDate()
-        const weekDayStartNumber = new Date(year, i).getDay()
-        const weekDayStart = weekDayStartNumber === 0 ? 6 : weekDayStartNumber - 1
-        const days = []
-        for (let j = 0; j < daysCount; j++) {
-            const day = {
-                date: j + 1,
-                notices: [],
-            }
-            days.push(day)
-        }
-        res.push({
-            name,
-            days,
-            daysCount,
-            weekDayStart,
-        })
-    }
-    return res
-}
+// export const getYearMonths = (year) => {
+//     const y = parseInt(year)
+//     const res = []
+//     for (let i = 0; i < MonthNames.length; i++) {
+//         const name = MonthNames[i]
+//         const daysCount = new Date(y, i + 1, 0).getDate()
+//         const weekDayStartNumber = new Date(y, i).getDay()
+//         const weekDayStart = weekDayStartNumber === 0 ? 6 : weekDayStartNumber - 1
+//         const days = []
+//         for (let j = 0; j < daysCount; j++) {
+//             const day = {
+//                 date: j + 1,
+//                 notices: [],
+//             }
+//             days.push(day)
+//         }
+//         res.push({
+//             name,
+//             days,
+//             daysCount,
+//             weekDayStart,
+//         })
+//     }
+//     return res
+// }
 
-export const getDaysWithIndent = (month) => {
-    const indentDays = []
-    for (let i = 0; i < month.weekDayStart; i++) {
-        indentDays.push(`indentDay${i}`)
-    }
-    return [...indentDays, ...month.days]
-}
+// export const getDaysWithIndent = (month) => {
+//     const indentDays = []
+//     for (let i = 0; i < month.weekDayStart; i++) {
+//         indentDays.push(`indentDay${i}`)
+//     }
+//     return [...indentDays, ...month.days]
+// }
 
-export const checkYearInRange = (year) => {
-    const [from, to] = yearRange
-    if (isNaN(year)) {
-        return `/${new Date().getFullYear()}`
-    } else if (year >= from && year <= to) {
-        return false
-    } else if (year < from) {
-        return `/${from}`
-    } else if (year > to) {
-        return `/${to}`
-    }
-}
+// export const checkYearInRange = (year) => {
+//     const y = parseInt(year)
+//     const [from, to] = YearRange
+//     if (isNaN(y)) {
+//         return `/${new Date().getFullYear()}`
+//     } else if (y >= from && y <= to) {
+//         return false
+//     } else if (y < from) {
+//         return `/${from}`
+//     } else if (y > to) {
+//         return `/${to}`
+//     }
+// }
